4866c70a3dc4a7f8ed9897909d2c3f6ff2f91ec2
ElijahCoding <cuiyeqing960904@gmail.com>
2020-08-06 00:32:36 +0300

    templates + db

--- RealEstateTemplate/settings.py
+++ RealEstateTemplate/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'RealEstateTemplate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '54321',
     }
 }
 
